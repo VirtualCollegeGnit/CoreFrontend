6e521fa02d74e8513d7274df40cba4510edeec77
Ankur Nigam <ankur.nigam198@gmail.com>
2020-01-07 16:18:29 +0530

    core server url

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import axios from "axios";
 
-const API_URL = "http://localhost:5001";
+const API_URL = process.env.DEV ? "http://localhost:5001" : "https://virtualcollege-corebackend.herokuapp.com";
+// const API_URL = "https://virtualcollege-corebackend.herokuapp.com";
 axios.defaults.baseURL = API_URL;
 
 Vue.prototype.$axios = axios;
