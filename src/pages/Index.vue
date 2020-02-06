f12c302c641574e929dba4bc1116cd725f8bdb67
Ankur Nigam <ankur.nigam198@gmail.com>
2020-02-07 03:28:10 +0530

    removed quasar branding

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,26 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    <!-- <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" /> -->
+    <div>
+      <h1>Welcome to</h1>
+      <h2>Virtual College</h2>
+      <br />
+      <div>
+        <q-btn
+          color="black"
+          text-color="white"
+          label="Login"
+          size="lg"
+          v-if="!$oidc.isAuthenticated"
+        />
+        <q-btn
+          color="blue"
+          label="My Account"
+          size="lg"
+          v-if="$oidc.isAuthenticated"
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
